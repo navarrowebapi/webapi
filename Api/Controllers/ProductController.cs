32733ee56961360f994cccaa93d5715a67a38a8c
navarrowebapi <fpnav@yahoo.com.br>
2016-05-03 12:02:16 -0300

    post

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -10,66 +10,30 @@ namespace Api.Controllers
 {
     public class ProductController : ApiController
     {
-        List<Product> listaDeProdutos = new List<Product>();
-        
+        static List<Product> listaDeProdutos = new List<Product>
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
 
-        // GET: api/Product
+
         public IHttpActionResult Get()
         {
-            var produto1 = new Product{ Id = 1, Name = "Placa de Rede",
-                Category = "Hardware", Price = 120m};
-
-            var produto2 = new Product { Id = 2, Name = "Placa mãe", 
-                Category = "Hardware", Price = 480m };
-            
-            var produto3 = new Product { Id = 3, Name = "Antivirus",
-                Category = "Software", Price = 80m };
-
-            listaDeProdutos.Add(produto1);
-            listaDeProdutos.Add(produto2);
-            listaDeProdutos.Add(produto3);
-
             return Ok(listaDeProdutos);
         }
 
-        // GET: api/Product/5
         public IHttpActionResult Get(int id)
         {
-            var produto1 = new Product
-            {
-                Id = 1,
-                Name = "Placa de Rede",
-                Category = "Hardware",
-                Price = 120m
-            };
-
-            var produto2 = new Product
-            {
-                Id = 2,
-                Name = "Placa mãe",
-                Category = "Hardware",
-                Price = 480m
-            };
-
-            var produto3 = new Product
-            {
-                Id = 3,
-                Name = "Antivirus",
-                Category = "Software",
-                Price = 80m
-            };
-
-            listaDeProdutos.Add(produto1);
-            listaDeProdutos.Add(produto2);
-            listaDeProdutos.Add(produto3);
-
             var produto = listaDeProdutos.Find(x => x.Id == id);
             return Ok(produto);
         }
 
         // POST: api/Product
-        public void Post([FromBody]string value)
+        public IHttpActionResult Post([FromBody] Product produto)
         {
+            listaDeProdutos.Add(produto);
+            return Ok();
         }
 
         // PUT: api/Product/5
